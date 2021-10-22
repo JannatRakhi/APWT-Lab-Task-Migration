4ace968d9c3e6194e2ed1113795434bade6ced54
JannatRakhi <jannatrakhi31155@gmail.com>
2021-10-22 17:14:11 +0600

    completed

---
+++ resources/views/layouts/navBar.blade.php
@@ -0,0 +1,14 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
+  <a class="fw-bold py-2  px-5 navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ms-auto ">
+      <a class="fw-bold nav-item mx-4 nav-link active" href="{{ route ('home') }}">Home</a>
+      <a class="fw-bold nav-item mx-5 nav-link active " href="{{ route ('productList') }}">Products</a>
+     
+    </div>
+  </div>
+</nav>
